044956df4512e0eb92beae944f6aa3cbfe9ae2da
qnduong <qnduong@talentsoft.com>
2021-02-27 23:12:38 +0100

    Add IsOverbought & IsOversold properties for StochResult

--- indicators/Stoch/Stoch.Models.cs
+++ indicators/Stoch/Stoch.Models.cs
@@ -8,5 +8,7 @@ namespace Skender.Stock.Indicators
         public decimal? Oscillator { get; set; }
         public decimal? Signal { get; set; }
         public decimal? PercentJ { get; set; }
+        public bool? IsOverbought => Signal == null ? (bool?)null : Signal.Value > 80;
+        public bool? IsOversold => Signal == null ? (bool?)null : Signal.Value < 20;
     }
 }
